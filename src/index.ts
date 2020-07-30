dad3b00f6e37629e2728a9616580acfafe8a60d8
Ivan Baena <baenadev.io@gmail.com>
2020-07-30 01:28:13 -0400

    refactor composition approah

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const matchReader = MatchReader.fromCsv('football.csv');
+matchReader.load();
 
-let manUnitedWins = 0;
+const summary = Summary.winsAlysisWithHtmlReport('Man United');
 
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins}`);
+summary.buildAndPrintreport(matchReader.matches);
